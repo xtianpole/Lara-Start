f1cc01dfddd9da3bdb47cb68dd729047608fc813
Your Namextianpole <xtianpole@gmail.com>
2018-10-16 22:13:25 +0800

    initial commit

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -18,13 +18,15 @@
                     <th>Name</th>
                     <th>Email</th>
                     <th>Type</th>
+                    <th>Created Date</th>
                     <th>Modify</th>
                   </tr>
-                  <tr>
-                    <td>183</td>
-                    <td>John Doe</td>
-                    <td>11-7-2014</td>
-                    <td><span class="tag tag-success">Approved</span></td>
+                  <tr v-for="user in users" :key="user.id">
+                    <td>{{user.id}}</td>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                     <td>{{user.type | upText}}</td>
+                     <td>{{user.created_at | myDate}}</td>
                     <td>
                         <a href="" >
                             <i class="fa fa-edit blue"></i>
@@ -114,6 +116,7 @@
     export default {
         data(){
             return{
+                users :{},
                 form:new Form({
                     name : '',
                     email : '',
@@ -127,12 +130,16 @@
 
         },
         methods: {
+            loadUsers(){
+                 axios.get("api/user").then(({ data }) => (this.users = data.data));
+
+            },
             createUser(){
                  this.form.post('api/user');
             }
         },
-        mounted() {
-            console.log('Component mounted.')
+       created() {
+            this.loadUsers();
         }
     }
 </script>
